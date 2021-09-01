a3ddf2b162db12ed934d254e8f7301ce71fd67d0
Saad <saad.wicks@gmail.com>
2021-09-01 19:36:25 +0500

    Full Complete index.html

--- js/app.js
+++ js/app.js
@@ -39,3 +39,47 @@ years = years.getFullYear();
 year.innerHTML = `<h5>${years} wear up ©<h5>`;
 /* Year set */
 
+/* Burger setup */
+const burger = document.querySelector(".burger-btn");
+const menuContainer = document.querySelector(".header-container2");
+
+burger.addEventListener("click", function(){
+    menuContainer.classList.toggle("menu-show");
+});
+
+/* Burger setup end */
+
+
+/* Search setup */
+const searchBar = document.querySelector(".search-bar");
+let headerRight = document.querySelector('.header-right');
+let searchHead = document.querySelector('.search-head');
+
+searchHead.innerHTML = `<input class="show-input" type="text" placeholder="Search">
+<button class="search-btn-head"><span class="iconify search-icon"
+        data-icon="bx:bx-search-alt"></span></button>`;
+
+searchBar.innerHTML = `<input type="text" placeholder="Search">
+<button class="search-btn"><span class="iconify search-icon"
+        data-icon="bx:bx-search-alt"></span></button>`;
+
+
+
+
+    // containerWidth = window.screen.width;
+    // checkWidth = 690;
+    // if(containerWidth <= checkWidth){
+    //     headerRight.innerHTML = `<button class="search-head"><span class="iconify search-icon"
+    //             data-icon="bx:bx-search-alt"></span></button>`;
+    // }else{
+    //     headerRight.innerHTML = ``;
+    // }
+
+/* Search setup show on header*/
+const searchShow = document.querySelector('.search-btn-head');
+const searchInput = document.querySelector('.search-head input');
+
+searchShow.addEventListener("click" ,function(){
+    searchInput.classList.toggle('show-input');
+})
+
